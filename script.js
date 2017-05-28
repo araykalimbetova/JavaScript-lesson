d79b819d25cde1d54e8df073bc1c6f9817b93c7e
aray <aray.kalimbetova@mail.ru>
2017-05-28 23:08:02 +0600

    Домашнее задание-1 'Переменные и типы данных'

---
+++ script.js
@@ -0,0 +1,7 @@
+var a, b ,c ,result;
+a = 0.5;
+b = '33.8';
+c = 5.7;
+b = parseFloat(b);
+result = a + b + c;
+document.write('<p style="color:blue; font-style:italic;">Результат сложения чисел:' + a + '+' + b + '+' + c + '= <span style="color:red; font-style:bold;">'+ result + '</span> </p>');
