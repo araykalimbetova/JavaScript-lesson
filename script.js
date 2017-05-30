3113436b36be97037ce8b01478d659e1bfbf5478
aray <aray.kalimbetova@mail.ru>
2017-05-30 15:22:51 +0600

    Домашнее задание-2 'Циклы'

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ b = parseFloat(b);
 result = a + b + c;
 document.write('<p>Результат сложения чисел:' + a + '+' + b + '+' + c + '= <span>'+ result + '</span> </p>');*/
 
-
+/*
 //1.home-1
 var test, result, type;
 
@@ -50,4 +50,62 @@ else {
 	end_month = end_month + " дней";
 }
 
-document.write("<h2>До конца месяца осталось - " + end_month + "</h2>");
+document.write("<h2>До конца месяца осталось - " + end_month + "</h2>");*/
+
+var x = 2048;
+
+while (x > 0) {
+	x = x / 2;
+	
+	if(x%2 !=0) {
+		continue;
+	}
+	if(x == 0){
+		break;
+	}
+	document.write(x + "<br>");
+}
+
+
+var color_tr,color_td;
+document.write("<table border='1px'>");
+
+for(var i = 1; i < 10; i++) {
+	
+	if(i == 1) {
+		color_tr = "#c3c3c3";
+	}
+	else {
+		color_tr = "#ffffff";
+	}
+	
+	document.write("<tr style='height:30px;background-color:" + color_tr + "'>");
+
+	for(var j = 1; j < 10; j++) {
+		
+		if(j == 1 || i == 1) {
+			color_td = "#c3c3c3";
+		}
+		else {
+			color_td = "#ffffff";
+		}
+			
+		document.write("<td style='width:30px;background-color:" + color_td + "'>" + i*j + "</td>");
+	}
+	document.write("</tr>");
+}
+
+document.write("</table>");
+
+
+
+
+
+
+
+
+
+
+
+
+
